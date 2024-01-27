ce3ad5881bfb409972b703d1b34927652ff3f6a4
fq51 <elesinigorek@gmail.com>
2024-01-27 10:40:12 +0300

    Initial

---
+++ src/MainFrame.java
@@ -0,0 +1,20 @@
+import javax.swing.*;
+
+public class MainFrame extends JFrame {
+    private JPanel panel1;
+    private JTextField textField1;
+    private JTextField textField2;
+    private JTextField textField3;
+    private JButton дальшеButton;
+    private JComboBox comboBox1;
+
+    public MainFrame(){
+        setContentPane(panel1);
+        setBounds(100,100,500,400);
+        setVisible(true);
+
+        comboBox1.addItem("Мужской");
+        comboBox1.addItem("Женский");
+
+    }
+}
